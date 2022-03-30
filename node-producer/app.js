7bac943307d1da806d9c0c00d96f173156ae9041
Fedor Chervyakov <fchervyakov@gmail.com>
2022-03-30 18:41:58 +0300

    node-producer: add app.js

---
+++ node-producer/app.js
@@ -0,0 +1,20 @@
+import { createClient } from 'redis';
+
+(async () =>  {
+    const client = createClient({
+        url: 'redis://default:averystrongpassword@redis:6379'
+    });
+
+    client.on('error', (err) => console.log('Redis Client Error', err));
+
+    await client.connect();
+
+    await client.xAdd("mystream", "*", "name", "Sara", "surname", "OConnor");
+    await client.xAdd("mystream", "*", "name", "Fedor", "surname", "Chervyakov");
+
+    const l = await client.xLen("mystream");
+
+    console.log(l);
+
+    await client.disconnect();
+})();
